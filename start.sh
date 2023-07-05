36a2a0513bf8fca0b779b04f21b86ddbf9b9486d
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-07-05 19:13:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Gowdru123/rockersbot.git /rockersbot
+  git clone https://github.com/Gowdru123/supraHD.git /supraHD 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /rockersbot 
+  git clone $UPSTREAM_REPO /supraHD 
 fi
-cd /rockersbot 
+cd /supraHD 
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
